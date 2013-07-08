8f28735f42bd0e1d1e24ed0bab5d4e530ecda368
Mohsen Namazi <infinitebrew@gmail.com>
2013-07-08 19:23:00 +0000

    more changes to the webfile so that it picks up index.html

--- web.js
+++ web.js
@@ -3,13 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var fs = require('fs');
-  var fileName = fs.readFile("index.html");
-  var buf = new Buffer(fileName);
-  for (var i = 0; i < fileName.length; i++){
-    buf[i] = str.charCodeA(i);
-  }
-  response.send(buf);
+  response.send(index.html);
 });
 
 var port = process.env.PORT || 5000;
